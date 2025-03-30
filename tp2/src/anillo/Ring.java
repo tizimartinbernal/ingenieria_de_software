67a5288a3936f638991242ce8de7c1bf9298b397
Tiziano Levi Martin Bernal <tmartinbernal@udesa.edu.ar>
2025-03-30 17:59:00 -0300

    avances

--- tp2/src/anillo/Ring.java
+++ tp2/src/anillo/Ring.java
@@ -1,6 +1,5 @@
 package anillo;
 
-
 public class Ring {
     private static class Node {
         Object cargo;
@@ -8,7 +7,7 @@ public class Ring {
 
         Node(Object cargo) {
             this.cargo = cargo;
-            this.next = this;  // Por defecto, se apunta a sí mismo (circular)
+            this.next = this;  // Por defecto, se apunta a sí mismo (circular). Lo dejamos o lo sacamos y directamente lo inicializamos en add?
         }
     }
 
@@ -27,16 +26,45 @@ public class Ring {
         current = current.next;
         return this;
     }
+    
 
     public Object current() {
-        return null;
+        return current.cargo;
     }
 
     public Ring add( Object cargo ) {
-        return null;
+        if (size == 0) {
+            this.current = new Node(cargo);
+            this.size++;
+            return this.next();
+        }
+        Node viejo_next = current.next;
+        Node nuevo_nodo = new Node( cargo );
+        current.next = nuevo_nodo;
+        nuevo_nodo.next = viejo_next;
+        size++;
+        return this.next();
+
     }
 
     public Ring remove() {
-        return null;
+        if (size == 0) {
+            throw new RuntimeException("Ring is empty");
+        } else if (size == 1) {
+            this.current = null;
+            this.size = 0;
+            return this;
+        }
+        Node viejo_next = current.next;
+        for (int i = 0 ; i < size - 1 ; i++) {
+            current = current.next;
+        }
+        current.next = viejo_next;
+        size--;
+        return this.next();
     }
 }
+// cuando hacemos add tenemos que avanzar el current?
+// está bien el test 8?
+// Está bien nuestra implementación con current solamente o deberíamos tener head y last. Porque si no tenemos head y last o si no tenemos la lista doblemente enlazada el remove nos queda en O(n)
+// está bien nuestra idea del add y del remove? Porque nos anda casi todos pero el 8 y el 12 no.
