293ebb1dc1a003f40abefa648b8ec736eb67981b
Mateo Giacometti <101135942+Mateo-Giacometti@users.noreply.github.com>
2025-06-07 10:31:51 -0300

    Prueba de funcionamiento del proyecto (test y llamados `http`)

--- tp4/UnoBackend/src/main/java/org/udesa/unobackend/controller/UnoController.java
+++ tp4/UnoBackend/src/main/java/org/udesa/unobackend/controller/UnoController.java
@@ -1,4 +1,15 @@
 package org.udesa.unobackend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
 public class UnoController {
+    @PostMapping("newmatch") public ResponseEntity newMatch(@RequestParam List<String> players ) {
+        return ResponseEntity.ok(UUID.randomUUID().toString());
+    }
 }
